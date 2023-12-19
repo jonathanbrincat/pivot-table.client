6b97bd6939a546abbeea4b65849743d1a51b8418
jonathan <jonathan@pix8.co.uk>
2023-12-19 16:05:23 +0000

    provision bootstrap and bootstrap icons

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './data_explorer/root_component.jsx'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './data_explorer/css/main.css'
 
 ReactDOM
