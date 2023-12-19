3b27d91199d6e9a8af19f9a48d1650fb22f3e58e
jonathan <jonathan@pix8.co.uk>
2023-12-19 15:59:09 +0000

    modded pathings so things can meet and be happy

--- src/data_explorer/js/constants.js
+++ src/data_explorer/js/constants.js
@@ -1,3 +1,20 @@
+export const colors = [
+  '#FDC612', // Main color
+  '#8AD8C8', // Teal
+  '#FF79A1', // Pink
+  '#FD9843', // Orange
+  '#9D91E9', // Muted purple
+  '#4EBFEF', // Light blue
+  '#D472DE', // Pink-purple
+  '#5BBF77', // Green
+  '#FB7A7A', // Red
+  '#AEDE72', // Light green
+  '#3BB0B7', // Aqua
+  '#986EDD', // Purple
+  '#A1B74B', // Olive green
+  '#80A6EF', // Soft blue
+]
+
 export default {
   RENDERER: {
     table: 'Table',
